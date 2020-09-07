477753807c324c1fc0503bcc8ba693b180d2325a
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-09-07 21:03:07 +0530

    Update sgtemplate.tf

--- sgtemplate.tf
+++ sgtemplate.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "sgByTFE" {
   ingress {
-    from_port   = "${var.ports[1]}"
-    to_port     = "${var.ports[1]}"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
   }
 
   egress {
-    from_port   = "${var.ports[0]}"
-    to_port     = "${var.ports[0]}"
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
   }
 }
